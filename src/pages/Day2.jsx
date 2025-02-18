2d6be540f0c861c73973cf969f222ccd4ad3f746
ayub mahmud <roodyridar2@gmail.com>
2025-02-18 23:23:39 +0300

    udpdate second page

--- src/pages/Day2.jsx
+++ src/pages/Day2.jsx
@@ -1,11 +1,8 @@
 const Day2 = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
-            <h1 className="text-4xl font-bold text-green-600">Welcome to Day 2! 🚀</h1>
-            <p className="mt-4 text-lg text-gray-700">Keep going! You're doing great!</p>
-            <button className="mt-6 px-6 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600">
-                Continue Challenge
-            </button>
+        <div className="flex flex-col items-center justify-center min-h-screen  bg-amber-500">
+            <h1 className="text-4xl font-bold text-white">Welcome to Day 2! </h1>
+        
         </div>
     );
 };
